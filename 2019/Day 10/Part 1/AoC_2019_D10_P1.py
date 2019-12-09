8d68be909c2be2f46f910f5901246f346751cc08
Joth <joth@localhost.localdomain>
2019-12-09 13:20:20 +0100

    add Day 10

---
+++ 2019/Day 10/Part 1/AoC_2019_D10_P1.py
@@ -0,0 +1,20 @@
+# Advent of Code 2019, Day 10, Part 1
+# Author: Joth (https://github.com/joth00)
+
+from os import path
+
+
+def main():
+    text_input = get_raw_input()
+    pass
+
+
+def get_raw_input():
+    return open(retrieve_input_file_path(), 'r').read()
+
+
+def retrieve_input_file_path():
+    return path.join(path.dirname(__file__), 'input.txt')
+
+
+main()
